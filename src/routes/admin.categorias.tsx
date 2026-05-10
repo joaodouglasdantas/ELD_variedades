f0c2179989a103a2a0ef1584401578960f3de7ae
joaodouglasdantas02b <joaodouglasdantas02b@gmail.com>
2026-05-10 10:31:40 -0300

    fix: consertando bug

--- src/routes/admin.categorias.tsx
+++ src/routes/admin.categorias.tsx
@@ -9,7 +9,6 @@ export const Route = createFileRoute("/admin/categorias")({
   component: AdminCategorias,
 });
 
-function slugify(s: string) {
   return s.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^a-z0-9]+/g, "-").replace(/^-|-$/g, "");
 }
 
